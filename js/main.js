6130d09ddf286e6d88ca43ed8736c98875b3f54a
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-11-08 10:23:15 -0700

    removing comments

--- js/main.js
+++ js/main.js
@@ -3,18 +3,14 @@ async function getData() {
     let data = await response.json()
     return data
 }
-//getData().then(data => console.log(data.data))
 getData().then(data => showImg(data))
 
 function showImg(info) {
     let gifHTML = ''
-    //console.log(info.data[0].id)
     //https://i.giphy.com/media/${info.data.id}/giphy.mp4
 
     for (let a = 0; a < info.data.length; a++) {
-        //console.log(info.data[a].id)
         gifHTML += `<div><video autoplay loop muted><source type="video/webm" src="https://i.giphy.com/media/${info.data[a].id}/giphy.mp4"</video></div>`
     }
-    //console.log(gifHTML)
     document.getElementById('test').innerHTML = gifHTML;
 }
