6ed15437edf8429cd656505681a905854d3c253a
Noah <noq@noq.local>
2021-11-09 10:25:14 -0700

    made dynamic landing screen background

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
-// return
-async function getTrendingGifs() {
-    let response = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=TJeFFaL4XtftdIyxMHSAUNJPNpW9YmnB&limit=20&rating=g");
+
+async function getTrendingGifs(limit = 1) {
+    let response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=TJeFFaL4XtftdIyxMHSAUNJPNpW9YmnB&limit=${limit}`);
     let data = await response.json();
     return data;
 }
+
+async function getRandomGif() {
+    let response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=TJeFFaL4XtftdIyxMHSAUNJPNpW9YmnB&limit=${limit}`);
+    let data = await response.json();
+    return data;
+}
+
+async function getRandomGifs(limit = 1) {
+    let gifs = [];
+
+    return gifs;
+}
+
+function displayGif() {
+
+}
+
+
+
+let backgroundGifDisplay = document.getElementById('background-gif-display');
+
+getTrendingGifs(15).then((gifs) => {
+    console.log(gifs.data);
+    for (let gif of gifs.data) {
+        console.log(gif.images.preview.mp4);
+        backgroundGifDisplay.innerHTML += `
+            <div class='gif-container'>
+                <video autoplay loop muted class="video">
+                    <source type="video/webm" src="${gif.images.preview.mp4}">
+                </video>
+            </div>
+        `;
+    }
+});
