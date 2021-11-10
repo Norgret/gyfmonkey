1f5b18b10ea9868ca3551b806f7602db75ca4f64
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-11-10 09:08:17 -0700

    Added to the saving functions
    functions now can save selected gifs and when the favorites page is loaded it successfully shows the chosen gifs

--- js/favorites.js
+++ js/favorites.js
@@ -14,15 +14,36 @@ function favGif(id) {
     localStorage.setItem('stored_GIFS', JSON.stringify(savedGifs));
 }
 
-
+//function for clear the local storage for testing
 function clearStorage() {
     localStorage.removeItem('stored_GIFS');
 }
 
 function render() {
-    console.log('works')
+    let htmlArea = document.getElementById('testArea');
+    let htmlContent = '';
+    savedGifs.forEach(element => {
+        htmlContent += `<div class="video-view">
+        <video autoplay loop muted class="video">
+            <source type="video/webm" src="https://i.giphy.com/media/${element}/giphy.mp4">
+        </video>
+        <div class="video-content">
+            <button onclick="remove('${element}')">Remove</button>
+            </div>
+        </div>`;
+    });
+
+    htmlArea.innerHTML = htmlContent;
 }
-/* HTML code
+
+function remove(gifToGo) {
+    let index = savedGifs.indexOf(gifToGo)
+    savedGifs.splice(index, 1)
+    localStorage.setItem('stored_GIFS', JSON.stringify(savedGifs));
+    console.log(JSON.parse(localStorage.getItem('stored_GIFS')))
+    render()
+}
+/* HTML code to show the gifs when the page loads
 
 <script src="../js/favorites.js"></script>
 <script type="text/javascript">
