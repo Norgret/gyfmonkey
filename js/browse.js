e14e1bbbe945961136f9a2efd9d9a0114f1fc0b0
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2021-11-15 09:20:35 -0700

    more work on the browse function
    made it so if a search is made in a page that is not browse.html it redirects to the browse page with the search made.

--- js/browse.js
+++ js/browse.js
@@ -1,7 +1,24 @@
+/*
+<body onload="showResults()"> must be in the browse.html for it to work properly
+loadBrowsePage() function must be used on a search bar that is NOT in the browse page
+runSearch() function must only be used in the browse page
+*/
+
+
 async function getSearch(search, number = 50) { //the API call
     let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=TJeFFaL4XtftdIyxMHSAUNJPNpW9YmnB&q=${search}&limit=${number}&offset=0&rating=g&lang=en`)
     let data = await response.json()
     return data
+}
+function loadBrowsePage() { //runs when a search bar is used from a page that isn't browse.html
+    localStorage.setItem('searchTerm', document.getElementById('search').value);
+    document.location.href = 'http://127.0.0.1:5500/html/browse.html';
+}
+function showResults() { //runs when the page loads
+    if (localStorage.getItem('searchTerm') !== null) {//runs if the local storage is not empty(means the search was made from the browse.js page)
+        getSearch(localStorage.getItem('searchTerm')).then(data => showSearch(data));
+        localStorage.removeItem('searchTerm'); //clears the search term now that the user is on the browse page
+    }
 }
 function runSearch() { //Gets the data from the searchBar and then runs the showSearch function
     let searchIn = document.getElementById('search').value
